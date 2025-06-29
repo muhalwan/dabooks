dd497a721366c2de6ca5ea917dd0527fc9800045
Muhammad Alwan <73457972+muhalwan@users.noreply.github.com>
2025-06-29 16:27:28 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,19 +1,12 @@
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+
 const config = {
-  production: {
-    API_URL: 'https://active-konstance-muhalwan-1333f850.koyeb.app'
-  },
-  development: {
-    API_URL: 'http://localhost:5000'
-  }
-};
-
-const currentConfig = process.env.NODE_ENV === 'production' ? config.production : config.development;
-
-export default {
-  ...currentConfig,
+  API_URL: API_URL,
   API_TIMEOUT: 15000,
   defaultHeaders: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
   }
 };
+
+export default config;
