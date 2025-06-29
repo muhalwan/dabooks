39535e18542773f7ea2c91adb86435d0c2297d6a
Muhammad Alwan <73457972+muhalwan@users.noreply.github.com>
2025-06-29 16:20:56 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,12 +1,19 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
-
 const config = {
-  API_URL: API_URL,
+  production: {
+    API_URL: 'https://dabooks-api-c1dc5695b41d.herokuapp.com'
+  },
+  development: {
+    API_URL: 'http://localhost:5000'
+  }
+};
+
+const currentConfig = process.env.NODE_ENV === 'production' ? config.production : config.development;
+
+export default {
+  ...currentConfig,
   API_TIMEOUT: 15000,
   defaultHeaders: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
   }
 };
-
-export default config;
